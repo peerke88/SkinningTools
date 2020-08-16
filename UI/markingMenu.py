c8cdcf7feb9383604f11eb2fcec15296297be591
Perry <perryleijten@gmail.com>
2020-08-16 21:09:00 +0900

    markingmenu working and cleaned up

--- UI/markingMenu.py
+++ UI/markingMenu.py
@@ -66,51 +66,46 @@ def objectUnderMouse(margin = 4, selectionType = "joint"):
     return foundBone, boneName
         
 class ToolTipFilter(QObject):
-    def __init__(self, name = "MarkingMenu", parent = None):
+    def __init__(self, name = "MarkingMenu", isDebug = False, parent = None):
         super(ToolTipFilter, self).__init__(parent)
+        self.__debug = isDebug
         self.MenuName = name  
         self.popup = None 
         self.getBoneUnderMouse = False
-        # self.setMouseTracking(True)
-        print "test"
     
     def eventFilter(self, obj, event):
         modifiers = QApplication.keyboardModifiers()
         if modifiers == Qt.AltModifier:
             self.getBoneUnderMouse = False
             return False
-
         
         if ( event.type() == QEvent.MouseButtonPress and event.button() == 4):
             print "mousePressed"
             foundObjects = objectUnderMouse()
+            if self.__debug:
+                foundObjects = (True, "testBone")
             print foundObjects
 
             if foundObjects == (False, '') or foundObjects is False:
                 return False
             
-            
             mainWin = wrapinstance( long( OpenMayaUI.MQtUtil.mainWindow() ) )
 
-            self.popup = radialMenu(mainWin, foundObjects, self)
+            self.popup = radialMenu(mainWin, foundObjects)
             self.popup.showAtMousePos()
             return True
         
         if self.popup is not None:
-            # print QCursor.pos()
             self.popup.updateLine(QCursor.pos())
 
-        # do this on mouse press, show the menu and kill it when released, activate button under mouse!
         if (event.type() == QEvent.MouseButtonRelease and event.button() == 4): 
             print "mouse released"
             if self.popup is None:
                 return True
-
-            # gather information on object undre mouse? mayeb from global or scene position!
-            # pos = QCursor.pos()
-            # nPos = self.popup.graphicsView.mapFromGlobal(pos)
-            # print nPos
-            # self.popup.scene.itemAt(QPointF(nPos.x(), nPos.y()))
+            _curItem = self.popup.getActiveItem()
+            if _curItem is not None:
+                # this is the function of the button that is under the mouse
+                _curItem.runFunction()
 
             self.popup.hide()
             self.popup.deleteLater()
@@ -119,125 +114,154 @@ class ToolTipFilter(QObject):
 
 
 class radialMenu(QMainWindow):
-    brush = QBrush()
-    brush.setStyle( Qt.SolidPattern )
-    brush.setColor( QColor( 80, 230, 80, 255 ) ) 
-
+    _red = QColor( 208, 52, 52, 255 ) 
+    _green =  QColor(80, 230, 80, 255)
+    
     pen = QPen()
-    pen.setStyle( Qt.SolidLine )
-    pen.setWidth( 1 )
-    pen.setColor( QColor( 104, 104, 104, 255 ) ) 
+    brush = QBrush()
+    brush.setStyle(Qt.SolidPattern)
+    brush.setColor(_green)
 
+    __borders = { "default": "border: 1px solid black;",
+                  "hilite" : "border: 2px solid #50E650;"}
+    __geoSize = 800
     __radius = 70
-    def __init__(self, parent=None, inputObjects = [], parentObject = None, flags=Qt.FramelessWindowHint):
+    _availableSpaces = 8
+    def __init__(self, parent=None, inputObjects = [], flags=Qt.FramelessWindowHint):
         super(radialMenu, self).__init__(parent, flags)
-        self.__geoSize = 800
+        #@note: clean this part up!!
+        
         self.OrigPos = QPoint()
         self.mappingPos = QPoint(self.__geoSize*.5, self.__geoSize*.5)
+        self.uiObjects = []
+        self.__ActiveItem = None
         self.setFixedSize(self.__geoSize, self.__geoSize)
         self.checkState = cmds.softSelect(q=True, softSelectFalloff= True)
         self.inputObjects = inputObjects
+
         self.scene = QGraphicsScene()
-        self.parentObject = parentObject
-        self.graphicsView = QGraphicsView()
-        self.graphicsView.setScene( self.scene )
-        self.graphicsView.setSceneRect(self.frameGeometry())
-        self.graphicsView.setHorizontalScrollBarPolicy( Qt.ScrollBarAlwaysOff )
-        self.graphicsView.setVerticalScrollBarPolicy( Qt.ScrollBarAlwaysOff )
-        self.setCentralWidget(self.graphicsView)
-        self.graphicsView.setMouseTracking(True)
+        graphicsView = QGraphicsView()
+        graphicsView.setRenderHints(QPainter.Antialiasing | QPainter.SmoothPixmapTransform | QPainter.HighQualityAntialiasing)
+        graphicsView.setScene( self.scene )
+        graphicsView.setSceneRect(self.frameGeometry())
+        graphicsView.setHorizontalScrollBarPolicy( Qt.ScrollBarAlwaysOff )
+        graphicsView.setVerticalScrollBarPolicy( Qt.ScrollBarAlwaysOff )
+        graphicsView.setMouseTracking(True)
+
+        self.setCentralWidget(graphicsView)
         self.setAttribute(  Qt.WA_TranslucentBackground , True)
         self.setStyleSheet("QWidget{background-color: rgba(100,0, 0, 50);background : transparent; border : none}")
-
-        nPen = QPen()
-        nPen.setStyle( Qt.DotLine )
-        nPen.setWidth( 2 )
-        nPen.setColor( QColor( 208, 52, 52, 255 ) ) 
-        item = QGraphicsEllipseItem(-self.__radius + self.mappingPos.x(),-self.__radius + self.mappingPos.y(),self.__radius*2,self.__radius*2)
-        item.setPen(nPen)
-        self.scene.addItem(item)
-        
+        self.__drawUI()
         self._buildButtons()
-
-        baseColor = QColor(80, 230, 80, 255)
-        pen = QPen()
-        pen.setStyle(Qt.SolidLine)
-        pen.setWidth(5)
-        pen.setColor(baseColor)
     
-        brush = QBrush()
-        brush.setStyle(Qt.SolidPattern)
-        brush.setColor(baseColor) 
+    def _setPen(self, color, width, style):
+        self.pen.setStyle( style )
+        self.pen.setWidth( width )
+        self.pen.setColor( color ) 
 
+    def __drawUI(self):
+        self._setPen(self._red, 2, Qt.DotLine )
+        backgroundCircle = QGraphicsEllipseItem(-self.__radius + self.mappingPos.x(),-self.__radius + self.mappingPos.y(),self.__radius*2,self.__radius*2)
+        backgroundCircle.setPen(self.pen)
+        
+        self._setPen(self._green, 5, Qt.SolidLine )
         self.itemToDraw = QGraphicsLineItem()
-        self.itemToDraw.setPen(pen)
+        circles = []
+        for i in xrange(2):
+            circle = QGraphicsEllipseItem(-2.5 + self.mappingPos.x(),-2.5 + self.mappingPos.y(), 5, 5)
+            circle.setPen(self.pen)
+            circle.setBrush(self.brush)
+            circles.append(circle)
+        
+        self.itemToDraw.startCircle = circles[0]
+        self.itemToDraw.endCircle = circles[1]
+
+        self.itemToDraw.setPen(self.pen)
         self.itemToDraw.setPos(QPoint(0,0))
-        self.scene.addItem(self.itemToDraw)
+        
+        for item in [backgroundCircle, circles[0], circles[1], self.itemToDraw]:
+            self.scene.addItem(item)
 
     def updateLine(self, pos):
         self.itemToDraw.setLine(self.mappingPos.x(),self.mappingPos.y(), 
                                 pos.x() - self.OrigPos.x()+ self.mappingPos.x(), 
                                 pos.y() - self.OrigPos.y()+ self.mappingPos.y())
+        self.itemToDraw.endCircle.setPos(QPoint(pos.x() - self.OrigPos.x(), 
+                                                pos.y() - self.OrigPos.y()))
+        _hit = False
+        for item in self.uiObjects:
+            item.setStyleSheet(self.__borders["default"]) 
+        for coll in self.itemToDraw.collidingItems():
+            if isinstance(coll, QGraphicsEllipseItem):
+                continue
+            if not coll.widget() in self.uiObjects:
+                continue
+            coll.widget().setStyleSheet(self.__borders["hilite"]) 
+            self.__ActiveItem = coll.widget()
+            _hit = True
+
+        if not _hit:
+            self.__ActiveItem = None
+
+    def getActiveItem(self):
+        return self.__ActiveItem
 
     def rotateVec(self, origin, point, angle):
         qx = origin.x + cos(angle) * (point.x - origin.x) - sin(angle) * (point.y - origin.y)
         qy = origin.y + sin(angle) * (point.x - origin.x) + cos(angle) * (point.y - origin.y)
         return  OpenMaya.MVector(qx, qy, 0.0)
 
+    def __MMButton(self, inText, position, inValue = None, inFunction = None):
+        item = QLabel(inText)
+        item.setStyleSheet(self.__borders["default"]) 
+        item.setAlignment(Qt.AlignCenter)
+        w = QPainter().fontMetrics( ).width( item.text()) + 10
+        item.setGeometry(position.x-(w*.5), position.y-10.5, w, 21)
+        if inFunction is not None:
+            item.runFunction = partial(inFunction, item, inValue)
+        return item
+
+    def __MMCheck(self, inText, position, inVal = True, inFunction = None):
+        item = QCheckBox(inText)
+        item.setChecked(inVal)
+        item.setStyleSheet(self.__borders["default"]) 
+        w = QPainter().fontMetrics( ).width( item.text()) + 20
+        item.setGeometry(position.x-(w*.5)-2, position.y-11.5, w+4, 21)
+        if inFunction is not None:
+            item.runFunction = partial(inFunction, item)
+        return item
+
     def _buildButtons(self):
-        _availableSpaces = 8
-        #@note change these buttons to bone setup marking menu
-        angle = (pi/(_availableSpaces))*2
+        angle = (pi/(self._availableSpaces))*2
         origin = OpenMaya.MVector()
         basePos = OpenMaya.MVector(0,-self.__radius,0)
         offset = OpenMaya.MVector(self.mappingPos.x(), self.mappingPos.y(),0)
-        denom = 0
+        
         positionList = []
-        [positionList.append(self.rotateVec(origin, basePos, (i - denom)*angle) + offset) for i in xrange(_availableSpaces)]
-
+        [positionList.append(self.rotateVec(origin, basePos, i*angle) + offset) for i in xrange(self._availableSpaces)]
 
         # ---- label -----
-        item = QLabel(self.inputObjects[1])
-        item.setAlignment(Qt.AlignCenter)
-        w = QPainter().fontMetrics( ).width( item.text()) + 10
-        item.setGeometry(positionList[0].x-(w*.5),positionList[0].y-10.5, w, 21)
+        item = self.__MMButton(self.inputObjects[1], positionList[0])
         self.scene.addWidget(item)
-
 
         # ---- surfaceAware -----
-        item = QCheckBox("surface aware")
-        item.setChecked(self.checkState)
-        w = QPainter().fontMetrics( ).width( "surface aware") + 20
-        item.setGeometry(positionList[5].x-(w*.5),positionList[5].y-11.5, w, 23)
-        item.stateChanged.connect(self._setCheckState)
-
+        item = self.__MMCheck("surface aware",positionList[5],  self.checkState, self._setCheckState)
         self.scene.addWidget(item)
+        self.uiObjects.append(item)
 
         # ----- setVal -----
         _dict = {2:1, 3:.5, 6:0}
         for key, val in _dict.iteritems():
-            item = QLabel("set weight: %s"%val)
-            item.setAlignment(Qt.AlignCenter)
-            w = QPainter().fontMetrics( ).width( item.text()) + 10
-            item.setGeometry(positionList[key].x-(w*.5),positionList[key].y-10.5, w, 21)
+            item = self.__MMButton("set weight: %s"%val, positionList[key], val, self.__funcPressed)
             self.scene.addWidget(item)
-
-        # #move this over to the main menu?
-        # # ---- incrementalVal ---
-        # for i in xrange(9):
-        #     item = QLabel("increment: %s"%(.1*(i+1)))
-        #     item.setAlignment(Qt.AlignCenter)
-        #     w = QPainter().fontMetrics( ).width( item.text()) + 10
-        #     item.setGeometry(positionList[4].x-(w*.5),positionList[4].y-10.5, w, 21)
-        #     self.scene.addWidget(item)
-        #     positionList[4] += OpenMaya.MVector(0,23,0)
+            self.uiObjects.append(item)
         return True
 
-    def __funcPressed(self, *args):
-        print self.sender().text()
+    def __funcPressed(self, item, value, *args):
+        print item.text()
 
-    def _setCheckState(self):
-        self.checkState = self.sender().isChecked()
+    def _setCheckState(self, item, *args):
+        self.checkState = not item.isChecked()
         cmds.softSelect(e=True, softSelectFalloff= self.checkState)
 
     def showAtMousePos(self):
@@ -246,21 +270,42 @@ class radialMenu(QMainWindow):
         self.show()
         return True
 
-# to test the functions
-def forceInstallFilter():
-    filter = ToolTipFilter()
 
-    for x in xrange(OpenMayaUI.M3dView.numberOf3dViews()):    
-        view =  OpenMayaUI.M3dView() 
+class testWidget(QMainWindow):
+    #simple widget to install the eventfilter on to test the markingmenu 
+    def __init__(self, parent=None):
+        super(testWidget, self).__init__(parent)
+        mainWidget = QWidget()
+        self.setCentralWidget(mainWidget)
+        self.setWindowFlags(Qt.Tool)
 
-        OpenMayaUI.M3dView.get3dView(x, view)
-        viewWidget = wrapinstance(view.widget())
-        try:
-            viewWidget.removeEventFilter(view._customFilter)
-        except:
-            pass
-        view._customFilter = ToolTipFilter(parent = viewWidget)
-        viewWidget.installEventFilter(view._customFilter) 
+        filter = ToolTipFilter(isDebug=True, parent = self)
+        self.installEventFilter(filter)
+
+def showTest():
+    def get_maya_window():
+        for widget in QApplication.allWidgets():
+            try:
+                if widget.objectName() == "MayaWindow":
+                    return widget
+            except:
+                pass
+        return None
+
+    window_name = 'testWidget'
+    mainWindow = get_maya_window()
+
+    if mainWindow:
+        for child in mainWindow.children():
+            if child.objectName() == window_name:
+                child.close()
+                child.deleteLater()
+    window = testWidget(mainWindow)
+    window.setObjectName(window_name)
+    window.setWindowTitle(window_name)
+    window.show()
+
+    return window
 
 
 # @note: original Code:
