66dbf259db5dff7d70472038a4af8bba6b2d2058
perry leijten <perryleijten@gmail.com>
2021-02-13 12:40:47 +0900

    fix marking menu collision bug
    Thanks to Trevor creating a new collision function to derive the right objects

--- UI/SkinningToolsUI.py
+++ UI/SkinningToolsUI.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-__VERSION__ = "5.0.20210212"
+__VERSION__ = "5.0.20210213"
 
 from SkinningTools.UI.qt_util import *
 from SkinningTools.UI.utils import *
--- packageInstaller.py
+++ packageInstaller.py
@@ -49,7 +49,7 @@ from SkinningTools.UI import utils
 from SkinningTools.Maya import api 
 from maya import cmds
 
-__VERSION__ = "5.0.20210212"
+__VERSION__ = "5.0.20210213"
 
 class InstallWindow(QDialog):
     def __init__(self,scriptDir, parent = None):
