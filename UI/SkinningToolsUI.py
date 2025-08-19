335564b5d540be5340a43c2c054969565f94fea3
perry leijten <perryleijten@gmail.com>
2025-08-19 20:24:46 +0900

    added plugin maya 2026

--- UI/SkinningToolsUI.py
+++ UI/SkinningToolsUI.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-__VERSION__ = "5.0.20240512"
+__VERSION__ = "5.0.20250819"
 
 from SkinningTools.UI.qt_util import *
 from SkinningTools.UI.utils import *
--- packageInstaller.py
+++ packageInstaller.py
@@ -52,7 +52,7 @@ from maya import cmds
 
 import base64
 
-__VERSION__ = "5.0.20240512"
+__VERSION__ = "5.0.20250819"
 
 class InstallWindow(QDialog):
     def __init__(self,scriptDir, parent = None):
